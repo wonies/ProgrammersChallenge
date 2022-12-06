b59b48db8fc7e5acad6c554fcf9c05d68922cc01
WONIE <newwon@WONIE-MAC.local>
2022-12-06 17:39:57 +0900

    0539

---
+++ 1205_2.c
@@ -0,0 +1,46 @@
+/* 프로그래머스 챌린지  |   두개 뽑아서 더하기 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int* solution(int numbers[], size_t numbers_len) {
+    int num[201] = {0};
+    int temp;
+    int* answer;
+    int cnt=0;
+
+    for(int i=0; i<numbers_len-1; i++)
+    {
+        for(int j=i+1; j<numbers_len; j++)
+        {
+            temp = numbers[i] + numbers[j];
+            if (num[temp]==0)
+            {
+                num[temp] = 1;
+                cnt++;
+            }
+        }
+    }
+    answer = (int *)calloc(cnt, sizeof(int));
+    int idx = 0;
+    for(int i=0; i<201; i++)
+    {
+        if (num[i]==1)
+            answer[idx++] = i;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    int number[10] = {2, 1, 3, 4, 1};
+    int num2[5] = {5, 0, 2, 7};
+    int *ans = solution(num2, 4);
+
+    for(int i=0; i<5; i++)
+    {
+      printf("[%d]", ans[i]);
+    }
+        
+}
