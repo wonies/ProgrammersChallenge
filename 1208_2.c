c4f5bc46dcaf99997a43c50d8755757a34d4e596
WONIE <newwon@WONIE-MAC.local>
2022-12-07 19:15:13 +0900

    average

---
+++ 1208_2.c
@@ -0,0 +1,11 @@
+/* 프로그래머스 챌린지  |   평균 구하기 */
+
+#include <stdio.h>
+
+double solution(int arr[], size_t arr_len) {
+    double answer = 0;
+    for (int i=0; i<arr_len; i++)
+        answer += arr[i];
+    answer /= arr_len;
+    return answer;
+}
