7f9c20c781e40ac7abc144c45c812ecf52c1b8cf
WONIE <newwon@WONIE-MAC.local>
2022-12-07 19:17:27 +0900

    average

--- 1208_2.c
+++ 1208_2.c
@@ -4,7 +4,7 @@
 
 double solution(int arr[], size_t arr_len) {
     double answer = 0;
-    for (int i=0; i<arr_len; i++)
+    for (size_t i=0; i<arr_len; i++)
         answer += arr[i];
     answer /= arr_len;
     return answer;
