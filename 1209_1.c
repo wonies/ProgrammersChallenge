7e72c2eea10363cedda0bf09b2967b7d8073fd76
WONIE <newwon@WONIE-MAC.local>
2022-12-09 12:33:09 +0900

    collatz_e

--- 1209_1.c
+++ 1209_1.c
@@ -1,41 +1,22 @@
 /* 프로그래머스 챌린지  |   콜라츠 추측 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-int solution(int num) 
+int solution(int num)
 {
-    int ans = 0;
-    if (num == 1)
+    long long ans = num;
+    int cnt = 0;
+
+    if (ans == 1)
         return 0;
-    if (num % 2 == 0)
-    {
-        while (num == 1)
-        {
-            num /= 2;
-            ans++;
-            if (ans > 499)
-                return -1;
-        }
-    }
-    else
-    {
-        while (num == 1)
-        {
-            num = num * 3 + 1;
-            ans++;
-            if (ans > 499)
-                return -1;
-        }
-    }
-    return ans;
-}
 
-int main()
-{
-    int n1 = 6;
-    int n2 = 16;
-    int n3 = 626331;
-
-    printf("--1--%d\n, --2--%d\n, --3--%d\n", solution(n1), solution(n2), solution(n3));
+    while (1)
+    {
+        if (ans == 1)
+            return cnt;
+        if (ans % 2 == 0) ans/=2, cnt++;
+        else ans = ans * 3 + 1, cnt++;
+        if (cnt > 499)
+            return -1;
+    }
 }
