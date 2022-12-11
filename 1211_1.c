3e44d7ac4dcd9c08790f90fcb165448ff101bcd6
wonies <bluewoniee@gmail.com>
2022-12-11 11:23:17 +0900

    plusnum

--- 1211_1.c
+++ 1211_1.c
@@ -5,13 +5,8 @@
 int solution(int n) 
 {
     int ans = 0;
-    for (int i=0; n; ans/=10)
+    
+    for (; n; n/=10)
         ans += n % 10;
     return ans;
-}
-
-int main()
-{
-    int n = 123;
-    printf("%d\n", n);
 }
