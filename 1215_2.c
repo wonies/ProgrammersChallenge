8e5ad1a16a5db278f579f2a6116def6c00af1e00
wonies <bluewoniee@gmail.com>
2022-12-16 05:07:25 +0900

    midchar

---
+++ 1215_2.c
@@ -0,0 +1,21 @@
+/* 프로그래머스 챌린지  | 가운데 글자 가져오기 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* solution(const char* s) {
+    int len = strlen(s);
+    char* ans = (char*)calloc(2, 1);
+    char *cpy;
+    cpy = strdup(s);
+    if (len % 2 == 1)
+        ans[0] = cpy[len / 2];
+    else
+    {
+        ans[0] = cpy[len / 2 - 1];
+        ans[1] = cpy[len / 2];
+    }
+    
+    return ans;
+}
