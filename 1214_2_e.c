912694f175192f8e29c2bd63d308c0137796d839
wonies <bluewoniee@gmail.com>
2022-12-16 05:19:17 +0900

    sumtwin

---
+++ 1214_2_e.c
@@ -0,0 +1,24 @@
+/* 프로그래머스 챌린지  | 두 정수 사이의 합 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define swap {int temp;}
+
+long long solution(int a, int b) {
+    long long ans = 0;
+    
+    if (a > b)
+    {
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    for(int i=a; i<=b; i++)
+    {
+        if (a==b)
+            return a;
+        ans += i;
+    }
+    return ans;
+}
