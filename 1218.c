250e5656eb267cec46ab2bb00621915d43a4ff63
wonies <bluewoniee@gmail.com>
2022-12-19 12:30:05 +0900

    correctbrak

--- 1218.c
+++ 1218.c
@@ -5,17 +5,19 @@
 #include <string.h>
 
 bool solution(const char* s) {
-    bool ans = true;
+    bool ans = false;
     int cnt = 0;
+
     for(int i=0; i<strlen(s); i++)
     {
+        if (cnt < 0)
+            return ans;
         if (s[i] == '(')
             cnt++;
         else
             cnt--;
     }
     if (cnt == 0)
-        return ans;
-    return ans * 0;
+        return ans + 1;
+    return ans;
 }
-
