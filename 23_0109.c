7f5fa1dedde0ee37e303404cbc9993c87df73d36
wonies <bluewoniee@gmail.com>
2023-01-14 16:38:19 +0900

    themostnum

--- 23_0109.c
+++ 23_0109.c
@@ -11,14 +11,14 @@ int solution(const char* want[], size_t want_len, int number[], size_t number_le
         num_cnt += number[i];
     int len = (int)discount_len - 10;
 
-    for (int i=0; i<len + 1; i++)
+    for (int i=0; i<len + 1; i++)   
     {
         for (int v=0; v< (int)number_len; v++)
             cpy_num[v] = number[v];
         for (int j=i; j < i+num_cnt;j++)
         {
             int check = 0;
-            for(int k=0; k<want_len; k++)
+            for(int k=0; k < want_len; k++)
             {
                 if (strcmp(want[k], discount[j])!=0)
                     check = -1;
@@ -33,7 +33,6 @@ int solution(const char* want[], size_t want_len, int number[], size_t number_le
             else if (check == -1)
                 break ;
         }
-
         int all_clear = 0;
         for(int i=0; i<(int)number_len ;i++)
         {
---
+++ 23_0116.c
@@ -0,0 +1,54 @@
+/* 프로그래머스 챌린지  | 가장 큰 수 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int cmp(void *cmp1, void *cmp2)
+{
+    char str1[9];
+    char str2[9];
+    
+    int a = *(int *)cmp1;
+    int b = *(int *)cmp2;
+    int diff = 0;
+    
+    sprintf(str1, "%d%d", a, b);
+    sprintf(str2, "%d%d", b, a);
+    
+    diff = strcmp(str1, str2);
+    
+    return -diff;
+}
+
+char* solution(int numbers[], size_t numbers_len) 
+{
+ 
+    int cnt = 0;
+    qsort(numbers, numbers_len, sizeof(int), cmp);
+    for (int i=0; i<numbers_len; i++)
+    {
+        char arr[5];
+        sprintf(arr, "%d", numbers[i]);
+        cnt += strlen(arr);
+    }
+    char* ans = (char*)calloc(cnt + 1, 1);
+ 
+    if (numbers[0] == 0)
+    {
+        ans[0] = '0';
+        ans[1] = '\0';
+        return ans;
+    }
+    else
+    {
+        int k = 0;
+        for(int i=0; i<numbers_len; i++)
+        {
+            char temp[1001];
+            sprintf(temp, "%d", numbers[i]);
+            strcat(ans, temp);
+        }
+    }
+    return ans;
+}
